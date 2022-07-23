d0cfaa2bc89cce864b6c7c8677f33610afd01900
keisuke.matsumoto <keisuke.suke.san@gmail.com>
2022-07-23 13:46:11 +0900

    ボタン押下時のclickイベントを追加

--- 2vueBasic/test/src/components/MyButton.vue
+++ 2vueBasic/test/src/components/MyButton.vue
@@ -1,10 +1,17 @@
 <template>
-  <button>test</button>
+  <button @click="onClick">test</button>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
-export default class MyButton extends Vue {}
+export default class MyButton extends Vue {
+  @Prop()
+  public greet?: string;
+
+  public onClick() {
+    alert(this.greet);
+  }
+}
 </script>
