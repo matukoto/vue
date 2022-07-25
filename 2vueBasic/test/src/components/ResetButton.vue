9280e4b14098d15c37a64ba25a87f0c1479067ae
keisuke.matsumoto <keisuke.suke.san@gmail.com>
2022-07-25 23:44:17 +0900

    モデルバインドの実装

---
+++ 2vueBasic/test/src/components/ResetButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="onClick">Reset</button>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class ResetButton extends vue {
+  @Prop()
+  public initialValue!: string;
+
+  @Prop()
+  public value!: string;
+
+  @Emit()
+  public input(value: string) {
+    // 何もしない
+  }
+
+  public onClick() {
+    this.input(this.initialValue);
+  }
+}
+</script>
--- 2vueBasic/test/src/views/HomeView.vue
+++ 2vueBasic/test/src/views/HomeView.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="home">
     <p>{{ greetText }}</p>
-    <MyButton :greet="greetText" @click="onMyButtonClicked"></MyButton>
+    <p>
+      <MyButton :greet="greetText" @click="onMyButtonClicked"></MyButton>
+    </p>
+    <p>
+      <ResetButton initialValue="Hello" v-model="greetText"></ResetButton>
+    </p>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import MyButton from "@/components/MyButton.vue"; // @ is an alias to /src
+import ResetButton from "@components/ResetButton.vue";
 
 @Component({
   components: {
     MyButton,
+    ResetButton,
   },
 })
 export default class HomeView extends Vue {
