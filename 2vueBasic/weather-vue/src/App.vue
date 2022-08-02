a85f259ca40e7453c4c115ae9b3292a034a4f24c
keisuke.matsumoto <keisuke.suke.san@gmail.com>
2022-08-02 23:36:36 +0900

    さようなら

--- 2vueBasic/weather-vue/src/App.vue
+++ 2vueBasic/weather-vue/src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <h1>Hello</h1>
+  <p class="test">GoodBye</p>
 </template>
 
 <script lang="ts"></script>
-<style></style>
+<style>
+h1 {
+  color: red;
+}
+
+.test {
+  font-style: italic;
+  letter-spacing: 10px;
+}
+</style>
