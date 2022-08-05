d297c9c8f2ec712c2279749044326d266ec1f2f2
keisuke.matsumoto <keisuke.suke.san@gmail.com>
2022-08-06 04:15:15 +0900

    APIから値を取得

--- 2vueBasic/weather-vue/src/App.vue
+++ 2vueBasic/weather-vue/src/App.vue
@@ -1,9 +1,21 @@
 <template>
-  <Title />
+  <WetherTitle />
+  <WetherForm />
+  <button @click="getWeather">getWetherテスト</button>
 </template>
 
 <script lang="ts">
-import Title from "@components/WeatherTitle.vue";
-</script>
+// import WeatherTitle from "@components/WeatherTitle.vue";
+// import WetherForm from "@components/WeatherForm.vue";
+import axios from "axios";
 
+const getWeather = () => {
+  axios
+    .get(
+      "https://api.weatherapi.com/v1/current.json?key=84f5439c0b48419eb81103125220608&q=London&aqi=no"
+    )
+    .then((res) => console.log(res));
+};
+</script>
+//
 <style></style>
